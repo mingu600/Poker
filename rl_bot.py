c9c20c5c1ee35be1d2d28d8c0a7397ee02252246
Robert Gordan <rfgordan@gmail.com>
2016-10-03 00:14:33 -0400

    removed callbacks

--- rl_bot.py
+++ rl_bot.py
@@ -94,35 +94,6 @@ class Learner(object):
             self.last_action
             return action
 
-    def find_index(self, state):
-        # State is [Hand Strength, Button Position, Opponent Aggressiveness, Opponent Action]
-
-        a = state[1]
-        b = math.floor(3 * state[1])
-        c = state[3]
-        d = math.floor(10 * state[0] - 0.0001)
-
-        return 1000 * a + 100 * b + 10 * c + d
-
-    def action_callback(self, state):
-        index = self.find_index(state)
-
-        old_action = self.Q[self.find_index(self.last_state)][self.last_action]
-        #print self.find_index(self.last_state) ,self.last_action
-
-        #changed from self.Q[index] = ...
-        #also debugged equation -- need to double-check
-        self.Q[self.find_index(self.last_state)][self.last_action] = old_action + self.alpha * (self.last_reward + self.gamma * np.argmax(self.Q[index]) - old_action)
-        self.last_action = np.argmax(self.Q[index])
-        #print self.last_action
-        self.last_state  = new_state
-        #print [self.Q[index][0], self.Q[index][1]]
-        return self.last_action
-
-    def reward_callback(self, reward):
-        '''This gets called so you can see what reward you get.'''
-        self.last_reward = reward
-
     #subroutine to record experiences
     def experience(self, state, action, reward, new_state):
         self.exp_writer.writerow(state + [action] + [reward] + new_state)
