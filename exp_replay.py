9abfc55041509ce8553e6be30a5cb7ad569c5ec4
Robert Gordan <rfgordan@gmail.com>
2016-11-26 14:23:56 -0800

    replay seems to maybe kinda work

--- exp_replay.py
+++ exp_replay.py
@@ -18,35 +18,47 @@ columns = 15
 chunksize =40
 file_size = 1000
 
+gamma = 0.5
+
 def load_models(model_path):
     training_model = load_model(model_path)
     target_model = load_model(model_path)
     return (training_model, target_model)
 
-# def load_data(experiences_path,target_model,epochs,chunksize):
-#
-# #iterate over dataset a certain number of times
-#     for epoch in range(epochs):
-#         #read in data in chunks
-#         df = pd.read_table(experiences_path,chunksize=chunksize)
-#         for chunk in df:
-#
-#             #separate into end-of-hand and not end-of-hand states
-#             chunk.iloc[:,reward] = chunk.iloc[:,reward] + (0 if chunk.iloc[:,reward+1].isNull() else ndarray.max(target_model.predict_on_batch(chunk.iloc[:,reward+1:]),axis=1))
-#             cols = list(chunk.columns)
-#             yield (chunk.iloc[:,:action].values,chunk.iloc[:,[action,reward]].values)
-
 def load_data(experiences_path,target_model,epochs,chunksize):
-    pd.read_csv(experiences_path)
+
+    #iterate over dataset a certain number of times
+    for epoch in range(epochs):
+        #read in data in chunks
+        df = pd.read_table(experiences_path,chunksize=chunksize,sep=',',header=None)
+        for chunk in df:
+
+            #separate into end-of-hand and not end-of-hand states
+            end = pd.isnull(chunk.iloc[:,reward+1])
+            end_chunk = chunk[end]
+            nend_chunk = chunk[~end]
+
+            #batch predict on new states
+            rewards_nend = target_model.predict_on_batch(nend_chunk.iloc[:,:action]).max(1) * gamma
+            chunk.loc[~end,reward] = rewards_nend
+
+            yield (chunk.iloc[:,:action].values,chunk.iloc[:,[action,reward]].values)
+
+# def load_data(experiences_path,target_model,epochs,chunksize):
+#     pd.read_csv(experiences_path)
 
 def train(train_model,data_gen,epochs,batches,m_path):
     for epoch in range(epochs):
         for batch in range(batches):
             b = next(data_gen)
             labels = train_model.predict_on_batch(b[0])
-            labels[b[1][:,0]] = b[1][:,1]
-            train_model.train_on_batch(b[0],labels, verbose=1)
-    model.save(m_path)
+
+            for row in b[1]:
+                print row[0]
+                labels[int(row[0])] = row[1]
+
+            train_model.train_on_batch(b[0],labels)
+    train_model.save(m_path)
 
 if __name__ == '__main__':
     m_path = sys.argv[1]
